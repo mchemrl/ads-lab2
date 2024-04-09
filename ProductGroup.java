6f61dbdb8f92e60ec00415fe1ac0bccb95e9d8be
diantsi <d.tsisaruk@ukma.edu.ua>
2024-04-09 21:20:27 +0300

    writing group names in the file vika dyrda i`m your lover

--- ProductGroup.java
+++ ProductGroup.java
@@ -1,12 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ProductGroup {
     private String name;
     private String description;
+    private static ArrayList<ProductGroup> groups = new ArrayList<>();
 
     public ProductGroup(String name, String description) {
         this.name = name;
         this.description = description;
+        groups.add(this);
+        writeGroupNamesToFile();
     }
 
+    public void writeGroupNamesToFile() {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("group_names.txt"));
+            for (ProductGroup group : groups) {
+                writer.write(group.getName());
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public String getName() {
         return name;
     }
